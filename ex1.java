74963a84df53ec5b1f7440e988cdb60304fde4a6
s1300230 <136041021+s1300230@users.noreply.github.com>
2023-06-09 12:22:58 +0900

    Dice game: initial revision

---
+++ ex1.java
@@ -0,0 +1,14 @@
+class Dice{
+    public static void main(String[] args){
+    
+        double r1 = Math.random()*6+1;
+        int a = (int)r1;
+        
+        double r2 = Math.ramdom()*6+1;
+        int b =(int)r2;
+        
+        System.out.println("Die 1: "+a);
+        System.out.println("Die 2: "+b);
+        System.out.println("Total value: "+(a+b));
+    }
+}
