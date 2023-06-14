34922f38459af08ac1b8ddaa0f8f10836bd28ffb
s1300230 <136041021+s1300230@users.noreply.github.com>
2023-06-15 00:40:50 +0900

    Won/lost message modified

--- Dice.java
+++ Dice.java
@@ -17,7 +17,7 @@ class Dice{
         double r1 = Math.random()*6+1;
         int a = (int)r1;
         
-        double r2 = Math.ramdom()*6+1;
+        double r2 = Math.random()*6+1;
         int b =(int)r2;
         
         System.out.println("Die 1: "+a);
@@ -26,9 +26,9 @@ class Dice{
         
         //
         if((a+b)>=7){
-            System.out.println("You won");
+            System.out.println(name+" won");
         }else{
-            System.out.println("You lost");
+            System.out.println(name+" lost");
         }
         //
     }
